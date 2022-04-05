8a894e9c5c11c895a153a1730f1a0bbdb1faf80f
Pedro Medina Faria <pedromedina190@gmail.com>
2022-04-05 12:48:31 -0400

    change text

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Love u Malala <3',
             {
                 font: font,
                 size: 0.5,
